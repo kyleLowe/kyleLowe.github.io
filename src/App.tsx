032935d3fd9342978e3fc768a108ac4c1ddbe653
kyleLowe <kylelowe358@gmail.com>
2025-09-12 17:23:14 +1200

    Added clickable links

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { int } from 'three/tsl';
 
 function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -39,6 +40,16 @@ function App() {
 59.59175271743869,
 29.97916640627271,
 63.47938369185648);
+    const raycaster = new THREE.Raycaster();
+    const pointer = new THREE.Vector2();
+    const raycasterObjects: THREE.Object3D<THREE.Object3DEventMap>[] = [];
+    let currentIntersects: string | any[] = [];
+
+    const socialLinks = {
+      Linkedin: "https://www.linkedin.com/in/kyle-lowe-90b232233/",
+      Github: "https://github.com/kyleLowe",
+    }
+
 
     // Add lights
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
@@ -62,6 +73,7 @@ function App() {
     controls.dampingFactor = 0.05;
     controls.update();
 
+    //Initial camera position
     controls.target.set(
 3.595150393092032,
 6.377301327597979,
@@ -78,20 +90,48 @@ function App() {
       loadedTextures[key] = tex;
     });
 
-     // Load the model
+    //Mouse object controls
+    window.addEventListener("mousemove", (event) => {
+    	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	  pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    });
+
+    window.addEventListener("click", (event) => {
+      if (currentIntersects.length > 0) {
+        const object = currentIntersects[0].object;
+
+        //Links to socials based on click
+        Object.entries(socialLinks).forEach(([name, url]) => {
+          if (object.name && object.name.includes(name)) {
+            const newWindow = window.open(url, '_blank', 'noopener,noreferrer');
+            if (newWindow) {
+              newWindow.opener = null;
+            }
+          }
+        }); 
+      }
+    }); 
+
+    // Load the model
     loader.load(
       '/models/RoomPortfolio.glb',
       (glb) => {
         glb.scene.traverse((child) => {
+          
           if ((child as THREE.Mesh).isMesh) {
             console.log('Mesh name:', child.name);
             (Object.keys(loadedTextures) as Array<keyof typeof loadedTextures>).forEach((key) => {
+              console.log(`Child name: ${child.parent?.name}, Checking for key: ${key}`);
               if (child.name.includes(key)) {
                 const material = new THREE.MeshStandardMaterial({
                   map: loadedTextures[key],
                   side: THREE.DoubleSide,
                 });
                 (child as THREE.Mesh).material = material;
+              }
+              if (child.name.includes("Raycaster") || child.parent?.name.includes("Raycaster")) {
+                raycasterObjects.push(child);
+                console.log('Added to raycaster objects:', child.name);
               }
             });
           }
@@ -123,6 +163,33 @@ function App() {
       cube.rotation.y += 0.01;
       // console.log('camera position:', camera.position);
       // console.log('controls position:', controls.target);
+      	// update the picking ray with the camera and pointer position
+      raycaster.setFromCamera( pointer, camera );
+
+      // calculate objects intersecting the picking ray
+      currentIntersects = raycaster.intersectObjects( raycasterObjects );
+
+      for ( let i = 0; i < currentIntersects.length; i ++ ) {
+
+        const mesh = currentIntersects[i].object as THREE.Mesh;
+        if (
+          mesh.material &&
+          'color' in mesh.material &&
+          mesh.material instanceof THREE.MeshStandardMaterial
+        ) {
+          mesh.material.color.set(0xff0000);
+        }
+
+      }
+      if (currentIntersects.length > 0) {
+        document.body.style.cursor = 'pointer';
+
+      }
+      else{
+        document.body.style.cursor = 'default';
+      }
+
+
       renderer.render(scene, camera);
       animationId = requestAnimationFrame(animate);
     };
