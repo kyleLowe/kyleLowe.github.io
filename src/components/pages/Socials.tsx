c69b4f1a2cbb41cce365fafa85c005f51d740e34
kyleLowe <kylelowe358@gmail.com>
2026-03-30 23:31:34 +1300

    Updated more colours and social

--- src/components/pages/Socials.tsx
+++ src/components/pages/Socials.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ModalHideButton from "../ModalHideButton";
 import type ModalProp from "../../interface/ModalInterface";
+import { Grid, Box } from "@mui/material";
 
 const Socials: React.FC<ModalProp> = ({ onHide }) => {
   return (
@@ -12,33 +13,63 @@ const Socials: React.FC<ModalProp> = ({ onHide }) => {
           <ModalHideButton onHide={onHide} />
         </div>
         <div className="window__body">
+          <Grid container>
+            <Grid size={4}>
+              <Box display="flex" flexDirection="column" alignItems="center">
+                {" "}
+                LinkedIn
+                <a
+                  href="https://www.linkedin.com/in/kyle-lowe-90b232233/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src="public/textures/projects/techicons/linkedin.svg"
+                    alt="LinkedIn"
+                    style={{ width: 128, height: 128, verticalAlign: "middle" }}
+                  />
+                </a>
+              </Box>
+            </Grid>
+            <Grid size={4}>
+              <Box display="flex" flexDirection="column" alignItems="center">
+                Personal Github
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/kyleLowe"
+                >
+                  <img
+                    src="public/textures/projects/techicons/github.svg"
+                    alt="GitHub"
+                    style={{ width: 128, height: 128, verticalAlign: "middle" }}
+                  />
+                </a>{" "}
+              </Box>
+            </Grid>
+            <Grid size={4}>
+              <Box display="flex" flexDirection="column" alignItems="center">
+                {" "}
+                University GitHub{" "}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/klow358"
+                >
+                  <img
+                    src="public/textures/projects/techicons/github.svg"
+                    alt="GitHub"
+                    style={{ width: 128, height: 128, verticalAlign: "middle" }}
+                  />
+                </a>{" "}
+              </Box>
+            </Grid>
+          </Grid>
           <p>
-            To contact me, message me on LinkedIn{" "}
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.linkedin.com/in/kyle-lowe-90b232233/"
-            >
-              here{" "}
-            </a>{" "}
             <br />
-            Check out my personal GitHub:{" "}
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/kyleLowe"
-            >
-              here{" "}
-            </a>{" "}
+
             <br />
-            Check out my university GitHub:{" "}
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/klow358"
-            >
-              here{" "}
-            </a>{" "}
+
             <br />
           </p>
         </div>
